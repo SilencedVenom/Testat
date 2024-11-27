4a371c4de3e1adf93cd61b4729eb7382329bccb1
edwin <edwindortmann@gmail.com>
2024-11-27 16:28:30 +0100

    added UserService.java

--- src/main/java/Services/UserService.java
+++ src/main/java/Services/UserService.java
@@ -1,4 +1,36 @@
 package Services;
 
+import Repository.UserRepository;
+import models.User;
+
+import java.sql.Timestamp;
+
 public class UserService {
+    private final RegexService regexService;
+    private final UserRepository userRepository;
+
+    public UserService(RegexService regexService) {
+        this.regexService = regexService;
+        this.userRepository = new UserRepository();
+    }
+
+    public boolean checkIfAccountExists(String email) {
+        User user = userRepository.findUserByEmail(email);
+        return user != null;
+    }
+
+    /**
+     *
+     * @param email
+     * @param password
+     */
+    public void registerUser(String email, String password) {
+        if (checkIfAccountExists(email)) {
+            new RuntimeException("Benutzer existiert bereits.");
+        } else {
+            User user = new User(-1, email, password, 1000, new Timestamp(System.currentTimeMillis()));
+            userRepository.addUser(user);
+        }
+    }
+
 }
