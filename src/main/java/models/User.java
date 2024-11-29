918b4c9e47572c6ed62d70cbd6f506ed10fbcf2a
Janes <janes.kuehne@hsw-stud.de>
2024-11-29 15:31:05 +0100

    Methode: showBalance gibt Kontostand aus

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -60,5 +60,9 @@ public class User {
         }
         return null;
     }
+
+    public void showBalance(User user){
+        System.out.println("Ihr aktueller Kontostand beträgt: " + user.getBalance());
+    }
 }
 
