fc85b01e9fa9d9b1c8a3c793fa892e8fc0807949
Flowe <florianhaberlandt1203mail.com>
2024-11-29 14:56:32 +0100

    UserService ausgebaut

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,5 +1,7 @@
 package models;
 
+import Repository.UserRepository;
+
 import java.sql.Timestamp;
 
 // User class representing the users table
@@ -42,6 +44,21 @@ public class User {
 
     public Timestamp getCreatedAt() {
         return createdAt;
+    }
+    public User login(String email, String password) {
+        UserRepository userRepository = new UserRepository();
+        User user = userRepository.findUserByEmail(email);
+        if (user != null) {
+            String storedPassword = user.getPassword();
+            if (storedPassword.equals(password)) {
+                return user;
+            } else {
+                System.out.println("Falsches Passwort.");
+            }
+        } else {
+            System.out.println("Benutzer nicht gefunden.");
+        }
+        return null;
     }
 }
 
