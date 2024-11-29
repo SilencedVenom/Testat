ef1a7a97157d440f7364813e822b6a83c42b41fa
edwin <edwindortmann@gmail.com>
2024-11-29 14:23:05 +0100

    added service for transactions

---
+++ src/main/java/Services/TransactionService.java
@@ -0,0 +1,46 @@
+package Services;
+
+import Exceptions.BalanceExceededException;
+import Exceptions.NegativeTransactionException;
+import Exceptions.UserNotFoundException;
+import Repository.UserRepository;
+import models.User;
+
+public class TransactionService {
+    public final RegexService regexService;
+    public UserRepository userRepository;
+    public User user;
+
+    public TransactionService(RegexService regexService, User user, UserRepository userRepository) {
+        this.regexService = regexService;
+        this.user = user;
+        this.userRepository = userRepository;
+    }
+
+    public void transactionToUser(String email, double balance) {
+        if (balance > user.getBalance()) {
+            throw new BalanceExceededException("Die zu überweisende Geldsumme übersteigt deinen Kontostand.");
+        }
+
+        if (!regexService.isValidEmail(email)) {
+            throw new UserNotFoundException("Die Email ist nicht vorhanden.");
+        }
+
+        User target = userRepository.findUserByEmail(email);
+        if (target == null) {
+            throw new UserNotFoundException("Der Benutzer ist nicht vorhanden.");
+        }
+
+        if (balance >= 0) {
+            target.setBalance(target.getBalance() + balance);
+            user.setBalance(user.getBalance() - balance);
+            userRepository.updateBalance(user.getEmail(), user.getBalance());
+            userRepository.updateBalance(target.getEmail(), target.getBalance());
+        } else {
+            throw new NegativeTransactionException("Du kannst keine negative Überweisung tätigen");
+        }
+
+    }
+
+
+}
