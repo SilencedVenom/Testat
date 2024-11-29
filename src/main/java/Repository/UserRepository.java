37fe93b432f54d646ada873ccd1c7482fcf2251d
edwin <edwindortmann@gmail.com>
2024-11-29 14:25:35 +0100

    comment fix

--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -39,7 +39,6 @@ public class UserRepository {
     /** Fügt einen User der Datenbank hinzu.
      * @param  {@link User}
      * @throws RuntimeException
-     * @throws SQLException
      */
     public void addUser(String email, String password, double balance, Timestamp created_at) {
         String query = "INSERT INTO users (email, password, balance, created_at) VALUES (?, ?, ?, ?)";
